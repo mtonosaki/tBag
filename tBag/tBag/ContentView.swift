c48b23b334789dca3e91f4daa1a4d6d0735dcfdb
Manabu Tonosaki <tonosaki@mail.toyota.co.jp>
2025-07-07 15:10:13 +0900

    add icons

--- tBag/tBag/ContentView.swift
+++ tBag/tBag/ContentView.swift
@@ -10,7 +10,7 @@ import SwiftData
 
 struct ContentView: View {
     @Environment(\.modelContext) private var modelContext
-    @Query private var items: [Item]
+    @Query private var items: [MemoItem]
 
     var body: some View {
         NavigationSplitView {
@@ -46,7 +46,7 @@ struct ContentView: View {
 
     private func addItem() {
         withAnimation {
-            let newItem = Item(timestamp: Date())
+            let newItem = MemoItem()
             modelContext.insert(newItem)
         }
     }
@@ -62,5 +62,5 @@ struct ContentView: View {
 
 #Preview {
     ContentView()
-        .modelContainer(for: Item.self, inMemory: true)
+        .modelContainer(for: MemoItem.self, inMemory: true)
 }
--- tBag/tBag/tBagApp.swift
+++ tBag/tBag/tBagApp.swift
@@ -12,7 +12,7 @@ import SwiftData
 struct tBagApp: App {
     var sharedModelContainer: ModelContainer = {
         let schema = Schema([
-            Item.self,
+            MemoItem.self,
         ])
         let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
 
