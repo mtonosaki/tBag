0eed91032ffe8a5f715cc577b14cf4819e576085
Manabu Tonosaki <manabu@tomarika.com>
2025-08-10 15:32:52 +0900

    change commit blocker message

--- check-email.sh
+++ check-email.sh
@@ -5,10 +5,6 @@ USER_EMAIL=$(git config user.email)
 if echo "$USER_EMAIL" | grep -q "manabu@tomarika.com"; then
     exit 0
 else
-    echo "--------------------------------------------------"
-    echo "エラー: コミットがブロックされました。"
-    echo ""
-    echo "現在の設定値: $USER_EMAIL"
-    echo "--------------------------------------------------"
+    echo "🈲 ERROR: COMMIT BLOCKED -- Current email: $USER_EMAIL"
     exit 1
 fi
